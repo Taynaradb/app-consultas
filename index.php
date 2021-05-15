a424186915aa79cd98538d345d8c7463a8f340d5
Taynara <73622125+Taynaradb@users.noreply.github.com>
2021-05-15 08:59:51 -0300

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php include "cabecalho.php"; ?>
-        <div class="conteudo d-flex justify-content-center align-items-center mt-5">
-            <h2>
-                Conteúdo da aplicação
-            </h2>
-        </div>
-<?php include "rodape.php"; ?>
